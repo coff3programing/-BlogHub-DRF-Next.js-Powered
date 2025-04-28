4e5049318e3f340b29575e600f374b9e831c131b
coff3programing <coff3programing@gmail.com>
2025-04-27 21:36:46 -0500

    New changes since BlogHub 📖

---
+++ core/views.py
@@ -0,0 +1,9 @@
+"""APIView"""
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class TestView(APIView):
+    def get(self, request):
+        return Response("Hello World")
